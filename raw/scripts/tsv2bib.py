eea3f445020155b4082c7accd24f43589fbbe11c
Kaj Syrjänen <43473945+kasyrj@users.noreply.github.com>
2018-10-09 15:20:56 +0300

    bibtex citations and bibtex-tsv conversion scripts

---
+++ raw/scripts/tsv2bib.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2
+# print bibtex entries from tsv Citation_codes.tsv to stdout. Run from raw folder.
+# To create bibliography, run `python scripts/tsv2bib.py > Citations.bib`
+
+import csv
+
+out = ""
+with open("Citation_codes.tsv","r") as f:
+    tsv = csv.reader(f, delimiter="\t",quotechar='"')
+
+    for row in tsv:
+        print('"' + row[2] + '"')
+        if row[2] == "type": # header
+            continue
+        if row[2] == "E":    # expert (not citable)
+            continue
+        
+        bibtex_entry = row[1].replace(", ",",\n ") + "\n\n"
+        out += bibtex_entry
+
+print(out)
+
