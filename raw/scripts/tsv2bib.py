c17000646290f967f92a36db3f3f1829969aa5eb
xrotwang <xrotwang@googlemail.com>
2018-10-09 15:30:29 +0200

    small tweak of the bibtex creation script

--- raw/scripts/tsv2bib.py
+++ raw/scripts/tsv2bib.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python2
 # print bibtex entries from tsv Citation_codes.tsv to stdout. Run from raw folder.
 # To create bibliography, run `python scripts/tsv2bib.py > Citations.bib`
-
+import re
 import csv
 
-out = ""
-with open("Citation_codes.tsv","r") as f:
-    tsv = csv.reader(f, delimiter="\t",quotechar='"')
+from pybtex.database import parse_string
 
-    for row in tsv:
+BREAK_PATTERN = re.compile(',\s+(?P<key>[a-z]+)\s*=')
+
+
+out = []
+with open("Citation_codes.tsv","r") as f:
+    for row in csv.reader(f, delimiter="\t",quotechar='"'):
         if row[2] == "type": # header
             continue
         if row[2] == "E":    # expert (not citable)
             continue
-        
-        bibtex_entry = row[1].replace(", ",",\n ") + "\n\n"
-        out += bibtex_entry
 
-print(out)
+        bib = BREAK_PATTERN.sub(lambda m: ',\n {0}='.format(m.group('key')), row[1])
+        parse_string(bib, bib_format='bibtex')
+        out.append(bib)
 
+print('\n\n'.join(out))
